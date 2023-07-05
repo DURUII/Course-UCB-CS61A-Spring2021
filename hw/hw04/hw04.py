3333cb5a3e1b3e0ee380ec2db80f1c263dd19360
DURUII <wust.durui@qq.com>
2023-07-05 19:57:56 +0800

    🪁hw04

--- hw/hw04/hw04.py
+++ hw/hw04/hw04.py
@@ -1,4 +1,5 @@
 HW_SOURCE_FILE = __file__
+from operator import mul, add, truediv, sub
 
 
 def mobile(left, right):
@@ -51,13 +52,13 @@ def end(s):
 def planet(size):
     """Construct a planet of some size."""
     assert size > 0
-    "*** YOUR CODE HERE ***"
+    return ['planet', size]
 
 
 def size(w):
     """Select the size of a planet."""
     assert is_planet(w), 'must call size on a planet'
-    "*** YOUR CODE HERE ***"
+    return w[1]
 
 
 def is_planet(w):
@@ -70,7 +71,7 @@ def examples():
                arm(2, planet(1)))
     u = mobile(arm(5, planet(1)),
                arm(1, mobile(arm(2, planet(3)),
-                              arm(3, planet(2)))))
+                             arm(3, planet(2)))))
     v = mobile(arm(4, t), arm(2, u))
     return (t, u, v)
 
@@ -117,7 +118,17 @@ def balanced(m):
     >>> check(HW_SOURCE_FILE, 'balanced', ['Index'])
     True
     """
-    "*** YOUR CODE HERE ***"
+    # plate
+    if is_planet(m):
+        return True
+
+    # mobile
+    # current layer: guard
+    if length(left(m)) * total_weight(end(left(m))) != length(right(m)) * total_weight(end(right(m))):
+        return False
+
+    # recursive call
+    return balanced(end(left(m))) and balanced(end(right(m)))
 
 
 def totals_tree(m):
@@ -149,11 +160,19 @@ def totals_tree(m):
     >>> check(HW_SOURCE_FILE, 'totals_tree', ['Index'])
     True
     """
-    "*** YOUR CODE HERE ***"
+    # plate
+    if is_planet(m):
+        return tree(total_weight(m))
+
+    # mobile
+    return tree(
+        total_weight(m),
+        [totals_tree(end(left(m))), totals_tree(end(right(m)))]
+    )
 
 
 def replace_thor_at_leaf(t, thors_replacement):
-    """Returns a new tree where every leaf value equal to "thor" has
+    """Returns a NEW tree where every leaf value equal to "thor" has
     been replaced with thors_replacement.
 
     >>> yggdrasil = tree('odin',
@@ -181,7 +200,13 @@ def replace_thor_at_leaf(t, thors_replacement):
     >>> laerad == yggdrasil # Make sure original tree is unmodified
     True
     """
-    "*** YOUR CODE HERE ***"
+    if is_leaf(t) and label(t) == "thor":
+        return tree(thors_replacement)
+
+    return tree(
+        label(t),
+        [replace_thor_at_leaf(branch, thors_replacement) for branch in branches(t)]
+    )
 
 
 def has_path(t, word):
@@ -215,7 +240,18 @@ def has_path(t, word):
     False
     """
     assert len(word) > 0, 'no path for empty word.'
-    "*** YOUR CODE HERE ***"
+
+    if label(t) != word[0]:
+        return False
+
+    if len(word) == 1:
+        return True
+
+    for branch in branches(t):
+        if has_path(branch, word[1:]):
+            return True
+
+    return False
 
 
 def preorder(t):
@@ -228,7 +264,16 @@ def preorder(t):
     >>> preorder(tree(2, [tree(4, [tree(6)])]))
     [2, 4, 6]
     """
-    "*** YOUR CODE HERE ***"
+    # base case
+    if is_leaf(t):
+        return [label(t)]
+
+    # preorder: root, branches
+    traversal = [label(t)]
+    for branch in branches(t):
+        traversal.extend(preorder(branch))
+
+    return traversal
 
 
 def interval(a, b):
@@ -238,12 +283,14 @@ def interval(a, b):
 
 def lower_bound(x):
     """Return the lower bound of interval x."""
-    "*** YOUR CODE HERE ***"
+    assert type(x) == list and len(x) == 2
+    return x[0]
 
 
 def upper_bound(x):
     """Return the upper bound of interval x."""
-    "*** YOUR CODE HERE ***"
+    assert type(x) == list and len(x) == 2
+    return x[1]
 
 
 def str_interval(x):
@@ -263,24 +310,28 @@ def add_interval(x, y):
 def mul_interval(x, y):
     """Return the interval that contains the product of any value in x and any
     value in y."""
-    p1 = x[0] * y[0]
-    p2 = x[0] * y[1]
-    p3 = x[1] * y[0]
-    p4 = x[1] * y[1]
-    return [min(p1, p2, p3, p4), max(p1, p2, p3, p4)]
+    return op_interval(mul, x, y)
+
+
+def op_interval(op, x, y):
+    p1 = op(lower_bound(x), lower_bound(y))
+    p2 = op(lower_bound(x), upper_bound(y))
+    p3 = op(upper_bound(x), lower_bound(y))
+    p4 = op(upper_bound(x), upper_bound(y))
+    return interval(min(p1, p2, p3, p4), max(p1, p2, p3, p4))
 
 
 def sub_interval(x, y):
     """Return the interval that contains the difference between any value in x
     and any value in y."""
-    "*** YOUR CODE HERE ***"
+    return op_interval(sub, x, y)
 
 
 def div_interval(x, y):
     """Return the interval that contains the quotient of any value in x divided by
     any value in y. Division is implemented as the multiplication of x by the
     reciprocal of y."""
-    "*** YOUR CODE HERE ***"
+    assert lower_bound(y) * upper_bound(y) > 0
     reciprocal_y = interval(1 / upper_bound(y), 1 / lower_bound(y))
     return mul_interval(x, reciprocal_y)
 
@@ -305,13 +356,14 @@ def check_par():
     >>> lower_bound(x) != lower_bound(y) or upper_bound(x) != upper_bound(y)
     True
     """
-    r1 = interval(1, 1)  # Replace this line!
-    r2 = interval(1, 1)  # Replace this line!
+    r1 = interval(1, 2)  # Replace this line!
+    r2 = interval(3, 4)  # Replace this line!
     return r1, r2
 
 
 def multiple_references_explanation():
-    return """The multiple reference problem..."""
+    return """not sure ... maybe right, because div_interval has tight assertions
+    while the condition will be examined multiple times in par2"""
 
 
 def quadratic(x, a, b, c):
@@ -323,7 +375,11 @@ def quadratic(x, a, b, c):
     >>> str_interval(quadratic(interval(1, 3), 2, -3, 1))
     '0 to 10'
     """
-    "*** YOUR CODE HERE ***"
+    f = lambda t: a * t ** 2 + b * t + c
+    candidates = [f(lower_bound(x)), f(upper_bound(x))]
+    if lower_bound(x) <= -b / (2 * a) <= upper_bound(x):
+        candidates.append(f(-b / (2 * a)))
+    return interval(min(candidates), max(candidates))
 
 
 # Tree ADT
