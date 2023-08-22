f115b4562b4607f0c1e1cd9dbd9e3b7462b61a05
DURUII <wust.durui@qq.com>
2023-08-23 01:14:52 +0800

    🌲lab10

--- lab/lab10/lab10.scm
+++ lab/lab10/lab10.scm
@@ -1,36 +1,47 @@
-(define (over-or-under num1 num2) 'YOUR-CODE-HERE)
+(define (over-or-under num1 num2) 
+    (cond 
+        ((< num1 num2) -1)
+        ((= num1 num2) 0)
+        ((> num1 num2) 1)
+    )
+)
 
-; ;; Tests
+;;; Tests
 (over-or-under 1 2)
-
 ; expect -1
 (over-or-under 2 1)
-
 ; expect 1
 (over-or-under 1 1)
-
 ; expect 0
-(define (make-adder num) 'YOUR-CODE-HERE)
 
-; ;; Tests
+(define (make-adder num) 
+    (lambda (inc) num + inc)
+)
+
+;;; Tests
 (define adder (make-adder 5))
-
 (adder 8)
-
 ; expect 13
-(define (composed f g) 'YOUR-CODE-HERE)
 
-(define lst 'YOUR-CODE-HERE)
+(define (composed f g) 
+    (lambda (x) (f (g x)))
+)
 
-(define (remove item lst) 'YOUR-CODE-HERE)
+(define lst 
+    (list (list 1) 2 (list 3 4) 5)
+)
 
-; ;; Tests
+(define (remove item lst) 
+    (filter 
+        (lambda (x) (not (= x item)))
+        lst
+    )
+)
+
+;;; Tests
 (remove 3 nil)
-
 ; expect ()
 (remove 3 '(1 3 5))
-
 ; expect (1 5)
 (remove 5 '(5 3 5 5 1 4 5 4))
-
 ; expect (3 1 4 4)
