026b1c63b1d883ef2f0950bb8bd3feece4768155
DURUII <wust.durui@qq.com>
2023-08-25 16:36:33 +0800

    🌲hw07

--- hw/hw07/hw07.scm
+++ hw/hw07/hw07.scm
@@ -1,4 +1,11 @@
-(define (filter-lst fn lst) 'YOUR-CODE-HERE)
+(define (filter-lst fn lst)
+  (cond 
+    ((null? lst)
+     lst)
+    ((fn (car lst))
+     (cons (car lst) (filter-lst fn (cdr lst))))
+    (else
+     (filter-lst fn (cdr lst)))))
 
 ; ;; Tests
 (define (even? x) (= (modulo x 2) 0))
@@ -6,7 +13,15 @@
 (filter-lst even? '(0 1 1 2 3 5 8))
 
 ; expect (0 2 8)
-(define (interleave first second) 'YOUR-CODE-HERE)
+(define (interleave first second)
+  (cond 
+    ((null? first)
+     second)
+    ((null? second)
+     first)
+    (else
+     (append (list (car first) (car second))
+             (interleave (cdr first) (cdr second))))))
 
 (interleave (list 1 5 3) (list 2 4 6))
 
@@ -18,6 +33,19 @@
 
 ; expect (1 2 3 4 5)
 (define (accumulate combiner start n term)
-  'YOUR-CODE-HERE)
+  (cond 
+    ((= 0 n)
+     start)
+    (else
+     (combiner (accumulate combiner start (- n 1) term)
+               (term n)))))
 
-(define (without-duplicates lst) 'YOUR-CODE-HERE)
+(define (without-duplicates lst)
+  (cond 
+    ((null? lst)
+     lst)
+    (else
+     (cons (car lst)
+           (without-duplicates
+            (filter-lst (lambda (x) (not (eq? x (car lst))))
+                        (cdr lst)))))))
